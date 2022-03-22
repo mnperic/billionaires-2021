d15a75617dd22bc5849c6ab0508fe2f9c3f150fe
Daniela <daniela.c.git@gmail.com>
2022-03-22 11:45:30 +1100

    templates update

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,65 +1,183 @@
-// Creating map object
-var myMap = L.map("map", {
-    center: [47.6038321, -122.3300624],
-    zoom: 10
-  });
+
+// create variables for the billionaires and counting data
+
+var bUrl ="../data/bgeojsoncountdc1.geojson";
+var cUrl = "../data/bgeojsoncountdc1.geojson";
+
+// read the data
+d3.json(bUrl, function(data) {
+  let bData = data.features
+  d3.json(cUrl, function(data) {
+    let countData = data.features
+
+    createMap(bData,countData)
+  })
+})
+// create map function
+function createMap(bData,countData) {
+// create variable for markers
+    let bMarkers = bData.map((feature) =>
+      L.circleMarker([feature.geometry.coordinates[1],feature.geometry.coordinates[0]],{
+          radius: nwCheck(feature.properties.NetWorth), 
+          stroke: true,
+          color: 'black',
+          opacity: 0.9,
+          weight: 0.9,
+          fill: true,
+          fillColor: nwColor(feature.properties.NetWorth), 
+          fillOpacity: 0.9   
+      })
+      .bindPopup("<h1> Net Worth : $" + feature.properties.NetWorth +
+      
+      "</h1><hr><h3> Name: " + feature.properties.Name +
+      "</h1><hr><h3> Age: " + feature.properties.Age +
+      "</h3><hr><p>" + (feature.properties.Country) + "</p>")
+    )
+
+
+
+// bilionaires markers
+    let billionaires = L.layerGroup(bMarkers);
+
+
+
+
+// xxxxxxxxxxxxxx
+    function makePolyline(feature, layer){
+      L.polyline([feature.geometry.coordinates[1],feature.geometry.coordinates[0]]
   
-  // Adding tile layer
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  );
+    }
+
+ // billionaires locations     
+    let bcounting = L.geoJSON(countData, {
+  
+      onEachFeature: makePolyline,
+        style: {
+          color: 'white',
+          opacity: 0.9
+        }
+    })
+
+
+
+// xxxxxxxxxxxx
+
+//   var heatpoints = {
+//   "radius": 2,
+//   "maxOpacity": .8,
+//   "scaleRadius": true,
+//   "useLocalExtrema": true,
+//   latField: feature.geometry.coordinates[1],
+//   lngField: feature.geometry.coordinates[0],
+//   valueField: feature.properties.Counts
+// };
+
+// xxxxxxxxxxxx
+
+// xxxxxxxxxxxx
+// let bcounting = L.HeatmapOverlay(heatpoints);
+
+//  var heatmappoints =[];
+// countData.features.forEachFeature(function(feature){
+// heatmappoints.push([feature.geometry.coordinates[1],feature.geometry.coordinates[0],feature.properties.Counts]);
+// console.log(heatmappoints)
+// var heat = L.heatLayer(heatmappoints, {radius:25}).addTo(myMap)
+
+// })
+
+// xxxxxxxxxxxx
+
+
+
+  // Define map layers
+  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
-  }).addTo(myMap);
-  
+  });
 
-// Define a markerSize function that will give each billionair a different radius based on its net worth
-function markerSize(NetWorth) {
-    return NetWorth *10;
-  }
-  
-  // Each billionair object contains the billionair's name, location and NetWorth
-  var billionaires = [
-    {
-      Name: "Jeff Bezos",
-      location: [47.6038321, -122.3300624],
-      NetWorth: 177.0
-    },
-    {
-      Name: "Elon Musk",
-      location: [30.2711286, -97.74369950000001],
-      NetWorth: 151.0
-    },
-    {
-      Name: "Bernard Arnault & family",
-      location: [48.8566969, 153.0260],
-      NetWorth: 150
-    },
-    {
-      Name: "Bill Gates",
-      location: [-20.7264, 2.3514616],
-      NetWorth: 124.0
-    },
-    {
-      Name: "Mark Zuckerberg",
-      location: [47.620548, -122.2264453],
-      NetWorth: 97.0
-    }
-  ];
-  
-  // Loop through the billionaires array and create one marker for each billionair object
-  for (var i = 0; i < billionaires.length; i++) {
-    L.circle(billionaires[i].location, {
-      fillOpacity: 0.75,
-      color: "darkblue",
-      fillColor: "green",
-      radius: 1000,
-      // Setting our circle's radius equal to the output of our markerSize function
-      // This will make our marker's size proportionate to its networth
-      radius: markerSize(billionaires[i].NetWorth)
-    }).bindPopup("<h1>" + billionaires[i].Name + "</h1> <hr> <h3>Net Worth: " + billionaires[i].NetWorth + "</h3>").addTo(myMap);
-  }
-  
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 19,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
 
+
+  var satellite =  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 19,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  });
+
+
+
+
+  var outdoors =  L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 19,
+    id: "outdoors-v11",
+    accessToken: API_KEY
+  });
+
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap,
+    "Satellite Map": satellite,
+    "Outdoors Map": outdoors
+  };
+
+  var overlayMaps = {
+    Billionaires_Net_Worth: billionaires,
+    Location : bcounting
+  };
+
+  var myMap = L.map("map", {
+    center: [47.6038321,-122.3300624],
+    zoom: 4,
+    layers: [streetmap, billionaires]
+  });
+
+var legend = L.control({ position: "bottomright" });
+
+legend.onAdd = function(myMap){
+    var div = L.DomUtil.create("div","legend");
+    div.innerHTML = [
+        "<k class='nwl1'></k><span>0-1</span><br>",
+        "<k class='nwl50'></k><span>1-50</span><br>",
+        "<k class='nwl100'></k><span>50-100</span><br>",
+        "<k class='nwl150'></k><span>100-150</span><br>",
+        "<k class='nwg150'></k><span>150+</span><br>"
+      ].join("");
+    return div;
+}
+
+legend.addTo(myMap);
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+}
+
+
+     function nwColor(NetWorth) {
+      var color = "";
+      if (NetWorth <= 1) { color = "gold"; }
+      else if (NetWorth <= 50) {color = "yellow"; }
+      else if (NetWorth <= 100) { color = "green"; }
+      else if (NetWorth <= 150) {color = "blue"; }
+      else { color = "darkblue"; }
+    
+    return color;
+    
+    };
+function nwCheck(NetWorth){
+  if (NetWorth <= 1){
+      return 0.2
+  }
+  return NetWorth * 0.2;
+}
