766b9414d0105898ae356c10ab9f1bf1037a47ca
gigi8android <gigi8android@gmail.com>
2022-04-09 14:30:49 +1000

    update mongdb atlas

--- bills_app.py
+++ bills_app.py
@@ -26,9 +26,8 @@ CORS(app)
 #################################################
 # Use PyMongo to establish Mongo connection for database mars_app
 #################################################
-mongo = PyMongo(app, uri="mongodb://localhost:27017/billionaires_app")
 
-
+mongo = PyMongo(app, uri=MONGODB_URI)
 #################################################
 # Route to render index.html template using data from MongoDB
 #################################################
